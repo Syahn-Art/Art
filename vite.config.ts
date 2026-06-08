885224784e89b913a6a3df03e4e005f6dbfa238f
Syahn Art <syahnart@gmail.com>
2026-06-08 20:15:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   // GitHub Pages 배포 시 리포지토리 이름을 base로 설정해야 합니다.
   // './'를 사용하면 상대 경로로 설정되어 대부분의 환경에서 호환됩니다.
  // base: "/art/",
-  base: "/",
+  base: "./",
   plugins: [react()],
   build: {
     outDir: "dist",
